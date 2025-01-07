ffeb0fabf8d735a18a42c367b44e01116fce4072
Will <wei6695@gmail.com>
2025-01-08 00:48:51 +0800

    20250108 修正註冊信箱正規表達式

--- HotelBookingSystem(飯店訂房系統)/HotelBookingSystem/obj/Debug/net8.0/HotelBookingSystem.AssemblyInfo.cs
+++ HotelBookingSystem(飯店訂房系統)/HotelBookingSystem/obj/Debug/net8.0/HotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e0278a1a1e9228e389ebd19eea581bf60601e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24be1e8b42f2bc10fb8c3c102a287b6619f2712f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
